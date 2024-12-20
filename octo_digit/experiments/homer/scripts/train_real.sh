a5d8e5d9b06d3ed95b882af0ebff7a743d925c50
Oier Mees <oier.mees@gmail.com>
2024-12-20 10:33:56 -0800

    remove experiments folder

--- octo_digit/experiments/homer/scripts/train_real.sh
+++
@@ -1,5 +0,0 @@
-export WANDB_MODE=disabled
-
-python scripts/train.py \
-    --config scripts/configs/octo_pretrain_config.py:vit_s \
-    --name test \
--- octo_digit/experiments/sudeep/aloha/standardization_transforms.py
+++
@@ -1,6 +0,0 @@
-from typing import Any, Dict
-
-
-def aloha_dataset_transform(trajectory: Dict[str, Any]) -> Dict[str, Any]:
-    trajectory["observation"]["proprio"] = trajectory["observation"]["state"]
-    return trajectory
